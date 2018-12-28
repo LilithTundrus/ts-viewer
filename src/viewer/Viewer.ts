95bcd2b9351ad1bb9b9cfc5ba480c0e45ee7e57a
Lilith Tundrus <echofox123@gmail.com>
2018-12-28 16:21:05 -0500

    Added barebones Viewer class file

---
+++ src/viewer/Viewer.ts
@@ -0,0 +1,18 @@
+// Using ES6 strict mode (not 100% needed, but ensure that the compiled JS is in strict mode)
+'use strict';
+
+// NPM dependencies
+
+// Local dependencies
+
+export default class Viewer {
+
+    constructor(filePath: string) {
+
+    }
+
+
+    start() {
+        
+    }
+}
