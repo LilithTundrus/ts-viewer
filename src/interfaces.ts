2327ab6d99432a6b497c12b73793bdf9cf2be3bf
Lilith Tundrus <echofox123@gmail.com>
2018-12-29 22:13:32 -0500

    Added file for containing interface references

---
+++ src/interfaces.ts
@@ -0,0 +1,7 @@
+// This interface contains all state options for TS-Edit
+export interface viewerState {
+    currentPath: string,
+    relativePath: string,
+    resolvedFilePath: string
+    fileName?: string
+}
