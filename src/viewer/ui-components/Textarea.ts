cebc13288e24ce9267f94fd95c94011109e16e28
Lilith Tundrus <echofox123@gmail.com>
2019-01-04 01:29:40 -0500

    Added a file for the textArea UI element

---
+++ src/viewer/ui-components/Textarea.ts
@@ -0,0 +1,9 @@
+// This file contains one of the blessed components for constructing the UI in an effort to
+// keep this project modular
+
+// Create the textArea textbox, where the text being edited will be displayed
+
+export default class TextArea {
+
+
+}
