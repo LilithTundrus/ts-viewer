b0ce41b9e831e1cf6a72f8aca36101999b1ec39b
Lilith Tundrus <echofox123@gmail.com>
2019-01-04 02:14:05 -0500

    Added a template file for handling keys on the textarea

---
+++ src/viewer/Keyhandler.ts
@@ -0,0 +1,45 @@
+// Using ES6 strict mode (not 100% needed, but ensure that the compiled JS is in strict mode)
+'use strict';
+
+// Node/NPM dependencies
+import Viewer from './Viewer';
+
+
+// Used for debugging
+import * as fs from 'fs';
+import { program } from 'blessed';
+
+// This file contains the class for handling key events for the viewer's class's
+// textArea UI component
+
+
+export default class KeyHandler {
+
+    constructor(viewerInstance: Viewer) {
+
+    }
+
+    leftArrowHandler() {
+
+    }
+
+    rightArrowHandler() {
+
+    }
+
+    upArrowHandler() {
+
+    }
+
+    downArrowHandler() {
+
+    }
+
+    homeHandler() {
+
+    }
+
+    endHandler() {
+
+    }
+}
